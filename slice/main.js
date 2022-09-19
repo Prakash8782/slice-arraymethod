364d2cca80a8df8209ee76eeefc3010c59a9a686
Prakash8782 <prakash8782@gmail.com>
2022-09-19 16:39:14 +0530

    slice

---
+++ slice/main.js
@@ -0,0 +1,6 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : ", colors);
+console.log("length of an array : ",colors.length);
+console.log("method : array.slice()");
+console.log("change in array :", colors.slice(1,-2));
+console.log("return : length of array after slice(); is "+colors.length);
